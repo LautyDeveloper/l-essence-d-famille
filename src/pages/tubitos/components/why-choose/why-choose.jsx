7227c37370db845e431809f0b6f4bdac30665ce4
Lautaro Aquino <laufabio24@gmail.com>
2025-09-22 15:58:32 -0300

    Terminamos seccion de Porque elegir Tubitos de 35 ML de la pagina Tubitos 35ML

--- src/pages/tubitos/components/why-choose/why-choose.jsx
+++ src/pages/tubitos/components/why-choose/why-choose.jsx
@@ -5,8 +5,8 @@ import "./why-choose.css";
 
 export default function WhyChoose() {
   return (
-    <div id="why-choose-container">
-      <h2>¿Por qué elegir el formato Tubitos?</h2>
+    <div id="why-choose-container" className="section">
+      <h2 className="title-section">¿Por qué elegir el formato Tubitos?</h2>
       <div className="why-choose-cards-container">
         {whyData.map((data) => {
           return (
